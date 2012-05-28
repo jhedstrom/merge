fdf3744144530547f2dfd142813941b35958e593
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-05-28 13:55:46 -0700

    Favicon and new bkg img.

--- js/merge.js
+++ js/merge.js
@@ -1,12 +1,13 @@
 $(function() {
   $.vegas('slideshow', {
     backgrounds: [
+      {src: '/img/both.jpg', valign: 'center'},
       {src: '/img/sarah.jpg', valign: 'top'},
       {src: '/img/jonathan.jpg', valign: 'top'}
     ],
     preload: true
   })
-  ('overlay', {src: '/js/vegas/overlays/06.png'});
+  ('overlay', {src: '/js/vegas/overlays/13.png'});
 
   $('.navbar').scrollspy();
 });
