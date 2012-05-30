9826568a8125c1210305658299627c5b66af97f6
Jonathan Hedstrom <jhedstrom@gmail.com>
2012-05-30 16:55:45 -0700

    Adding hayduke tongue for 404.

--- js/merge.js
+++ js/merge.js
@@ -1,14 +1,3 @@
 $(function() {
-  $.vegas('slideshow', {
-    backgrounds: [
-      {src: 'img/both.jpg', valign: 'center'},
-      {src: 'img/woods.jpg', valign: 'bottom'},
-      {src: 'img/sarah.jpg', valign: 'top'},
-      {src: 'img/jonathan.jpg', valign: 'top'}
-    ],
-    preload: true
-  })
-  ('overlay', {src: 'js/vegas/overlays/01.png'});
-
   $('.navbar').scrollspy();
 });
